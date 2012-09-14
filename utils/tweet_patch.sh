bf6d7fa0c2e37508ead66603c621df8128fd6b48
Santiago Basulto <santiago.basulto@gmail.com>
2012-09-14 11:05:12 -0300

    PUT and PATCH scripts

---
+++ utils/tweet_patch.sh
@@ -0,0 +1,1 @@
+curl --dump-header - -H "Content-Type: application/json" -X PATCH --data '{"tweet": "PATCHed tweet"}' http://127.0.0.1:8000/api/v1/tweet/3/
---
+++ utils/tweet_put.sh
@@ -0,0 +1,1 @@
+curl --dump-header - -H "Content-Type: application/json" -X PUT --data '{"tweet": "PUT tweet", "user": "/api/v1/user/2/"}' http://127.0.0.1:8000/api/v1/tweet/3/
