f363137d8d90495b1ec3ce6e9fcc1e86deb4d5e9
Santiago Basulto <santiago.basulto@gmail.com>
2012-09-15 10:01:18 -0300

    Basic filtering

--- pyday_rest/pyday_rest/api/resources.py
+++ pyday_rest/pyday_rest/api/resources.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 
 from tastypie import fields
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from tastypie.authorization import Authorization
 
 from twitter.models import Tweet
@@ -11,6 +11,11 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         excludes = ['password', 'is_staff', 'is_superuser']
+
+        filtering = {
+            'username': ALL,
+            'date_joined': ['range', 'gt', 'gte', 'lt', 'lte'],
+        }
 
 
 class TweetResource(ModelResource):
